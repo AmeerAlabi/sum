51ab8f6027468284f36653a1d688371bb41c6a60
ameer alabi <ameeralabi7@gmail.com>
2025-02-15 14:14:09 +0100

    edit

---
+++ src/components/Load.jsx
@@ -0,0 +1,16 @@
+import { BiLoaderCircle } from "react-icons/bi";
+import { motion } from "framer-motion";
+
+function Load() {
+    return (
+      <div className="flex justify-center items-center mt-4">
+        <motion.div
+          animate={{ rotate: 360 }}
+          transition={{ repeat: Infinity, duration: 1, ease: "linear" }}
+        >
+          <BiLoaderCircle size={40} className="text-blue-500" />
+        </motion.div>
+      </div>
+    );
+  }
+  
